f5f4448e8263f4fe34bf647d69574e0c5fc11da5
dydyFos <dylanfoster.esgi@gmail.com>
2018-03-02 16:33:48 +0100

    koa basic setup

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
+require('dotenv').config({ path: './.env.local' });
 const Koa = require('koa');
+const Router = require('koa-router');
+const Logger = require('koa-logger');
+const Cors = require('koa-cors');
+const Helmet = require('koa-helmet');
+const BodyParser = require('koa-bodyparser');
+const Respond = require('koa-respond');
+
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World !';
-});
+// Security middlewares
+app.use(Helmet());
+app.use(Cors({
+    origin: true,
+    'Access-Control-Allow-Methods': ['GET', 'PUT', 'POST', 'DELETE'],
+    'Access-Control-Allow-Headers': ['Content-Type', 'Authorization']
+}));
 
-app.listen(3000);
+// Parser middleware
+app.use(BodyParser({
+    enableTypes: ['json'],
+    jsonLimit: '5mb',
+    strict: true,
+    onerror(err, ctx) {
+        ctx.throw('Body parse error', 422, { err });
+    }
+}));
+
+if (process.env.NODE_ENV === 'dev') {
+    app.use(Logger());
+}
+
+// Response middleware
+app.use(Respond());
+
+// API routes
+require('./routes')(router);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(process.env.PORT || 3000);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Koa Todos API
+
+Based on https://github.com/dbalas/koalerplate
