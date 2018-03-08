92d1c7c2516438c39edd28bb540aae7a580f43a0
Roland Kullet <renaud.bellec.3@gmail.com>
2018-03-08 12:36:46 +0100

    Swagger up and ready

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 const DataBase = require("./db");
 const { devEnv } = require("./utils/consts");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+
+/* Swagger config */
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: "Hello World", // Title (required)
+            version: "1.0.0", // Version (required)
+        },
+    },
+    apis: [
+        "./routes/index.js",
+        "./routes/pokemons.js"
+    ], // Path to the API docs
+};
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+/* End Swagger config */
 
 const app = express();
 DataBase.connect();
@@ -17,6 +36,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/index"));
 app.use("/pokemons", require("./routes/pokemons"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,7 +50,7 @@ app.use(function(err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV === devEnv ? err : {};
-    
+
     // render the error page
     res.status(err.status || 500);
     res.render("error");
