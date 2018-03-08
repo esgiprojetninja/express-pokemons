ea26d73a06b0214ad80fa1670651a149359a3a8a
Roland Kullet <renaud.bellec.3@gmail.com>
2018-03-08 18:11:55 +0100

    added spies

--- tests/routes/pokemons.spec.js
+++ tests/routes/pokemons.spec.js
@@ -1,10 +1,17 @@
 require("dotenv").config({ path: "./.env.local" });
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const spies = require("chai-spies");
+const PokemonController = require("../../api/controllers/pokemonController");
+
 chai.use(chaiHttp);
+chai.use(spies);
 process.env.PORT = process.env.TEST_PORT || require("../../utils/consts").defaultTestPort;
 process.env.NODE_ENV = require("../../utils/consts").testEnv;
 const ROUTE_PREFIX = "pokemons";
+
+chai.spy.on(PokemonController, "list_all_pokemons", (req, res) => res.json([]));
+chai.spy.on(PokemonController, "create_pokemon", (req, res) => res.json(req.body));
 
 describe("pokemons routes", () => {
     let server = null;
@@ -13,33 +20,33 @@ describe("pokemons routes", () => {
     });
 
     it("should get and return an array", done => {
-        chai.request(server)
-            .get(`${ROUTE_PREFIX}/`)
+        return chai.request(server)
+            .get("/pokemons")
             .end((err, res) => {
-                console.log(res);
                 expect(err).toBeNull();
                 expect(res.status).toBe(200);
                 expect(res.ok).toBe(true);
                 expect(res.type).toBe("application/json");
-                expect(res.body).toEqual({ pokemons: [] });
+                expect(res.body).toEqual([]);
                 done();
             });
     });
 
     it("should post and create a pokemon", done => {
+        const pokemon = {
+            "name": "dydy",
+            "description": "Le plus crade de tous les pokemon",
+            "id_parent": 0,
+            "image": "prout",
+            "id_national": 1000,
+            "type1": 0,
+            "type2": 0,
+        };
         chai.request(server)
             .post(`/${ROUTE_PREFIX}`)
-            .send({
-                "name": "dydy",
-                "description": "Le plus crade de tous les pokemon",
-                "id_parent": 0,
-                "image": "prout",
-                "id_national": 1000,
-                "type1": 0,
-                "type2": 0,
-            })
+            .send(pokemon)
             .end((err, res) => {
-                expect(res.name).toBeNull();
+                expect(err).toBeNull();
                 expect(res.status).toBe(200);
                 expect(res.ok).toBe(true);
                 expect(res.type).toBe("application/json");
@@ -47,18 +54,6 @@ describe("pokemons routes", () => {
                 done();
             });
     });
-    it("should throw 404", done => {
-        chai.request(server)
-            .get(`/${ROUTE_PREFIX}/uhoh`)
-            .end((err, res) => {
-                expect(err).not.toBeNull();
-                expect(res.status).toBe(404);
-                expect(res.ok).toBe(false);
-                done();
-            });
-    });
-
-
 
     afterAll(async () => {
         await server.close();
