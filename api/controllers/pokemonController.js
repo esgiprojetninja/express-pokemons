113a3427be51c39fc5cbc3b95665b4d96c7f6bfe
GoRFy <lambot.rom@gmail.com>
2018-03-08 14:24:56 +0100

    oui

--- api/controllers/pokemonController.js
+++ api/controllers/pokemonController.js
@@ -4,60 +4,67 @@ let mongoose = require("mongoose"),
 attributesSelect = "id_pokemon description name id_parent image id_national type1 type2";
 
 /** list pokemons **/
-exports.list_all_pokemons = function(req, res) {
-    let query = Pokemon.find({}).select(attributesSelect).sort([["id_national", "ascending"]]);
-    query.exec(function (err, pokemons) {
-        if (err) return res.status(500).send(err);
-        res.json(pokemons);
-    });
+exports.list_all_pokemons = async function(req, res) {
+    try {
+        let query = Pokemon.find({}).select(attributesSelect).sort([["id_national", "ascending"]]);
+        const pokemons = await query.exec();
+        return res.json(pokemons);
+    } catch (error) {
+        return res.status(500).send(error);
+    }    
 };
 
 /** display pokemon **/
-exports.read_pokemon = function(req, res) {
-    let query = Pokemon.find({ id_national: req.params.Id }).select(attributesSelect);
-    query.exec(function (err, pokemons) {
-        if (err)
-            res.send(err);
-        res.json(pokemons);
-    });
+exports.read_pokemon = async function(req, res) {
+    try {
+        let query = Pokemon.find({ id_national: req.params.Id }).select(attributesSelect);
+        const pokemons = await query.exec();
+        return res.json(pokemons);
+    } catch (error) {
+        return res.status(500).send(error);
+    }    
 };
 
 /** create pokemon **/
-exports.create_pokemon = function(req, res) {
-    let new_pokemon = new Pokemon(req.body);
-    new_pokemon.save(function(err, pokemon) {
-        if (err)
-            res.send(err);
-        res.json(pokemon);
-    });
+exports.create_pokemon = async function(req, res) {
+    try {
+        let new_pokemon = new Pokemon(req.body);
+        const pokemons = await new_pokemon.save();
+        return res.json(pokemons);
+    } catch (error) {
+        return res.status(500).send(error);
+    }  
 };
 
 /** update pokemon **/
-exports.update_pokemon = function(req, res) {
-    Pokemon.findOneAndUpdate({ id_national: req.params.Id }, req.body, { new: true }, function(err, pokemon) {
-        if (err)
-            res.send(err);
-        res.json(pokemon);
-    });
+exports.update_pokemon = async function(req, res) {
+    try {
+        let query = Pokemon.findOneAndUpdate({ id_national: req.params.Id }, req.body, { new: true };
+        const pokemons = await query.exec();
+        return res.json(pokemons);
+    } catch (error) {
+        return res.status(500).send(error);
+    } 
 };
 
 /** delete pokemon **/
-exports.delete_pokemon = function(req, res) {
-    Pokemon.remove({
-        id_national: req.params.Id
-    }, function(err, pokemon) {
-        if (err)
-            res.send(err);
-        res.json({ message: "Pokemon successfully deleted" });
-    });
+exports.delete_pokemon = async function(req, res) {
+    try {
+        let query = Pokemon.remove({ id_national: req.params.Id };
+        const pokemons = await query.exec();
+        return res.json(pokemons);
+    } catch (error) {
+        return res.status(500).send(error);
+    } 
 };
 
 /** add a location **/
-exports.set_location = function(req, res) {
-    let new_location = new Location(req.body);
-    new_location.save(function(err, location) {
-        if (err)
-            res.send(err);
-        res.json(location);
-    });
+exports.set_location = async function(req, res) {
+    try {
+        let new_location = new Location(req.body);
+        const location = await new_location.save();
+        return res.json(location);
+    } catch (error) {
+        return res.status(500).send(error);
+    }  
 };
